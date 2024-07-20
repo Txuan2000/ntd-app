cd7c66894fb8c6a51ec4fc28df61be2e79b8d807
dungnt1907 <dungnt1@bitecco.vn>
2024-07-20 09:07:42 +0700

    fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'apps', pathMatch: 'full' },
   {
     path: 'apps',
     loadChildren: () => import('./applications/applications.routes').then(m => m.routes)
--- src/app/applications/applications.routes.ts
+++ src/app/applications/applications.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { GenTextComponent } from './gen-text/gen-text.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'gen-text', pathMatch: 'full' },
   { path: 'gen-text', component: GenTextComponent },
   { path: '**', redirectTo: 'applications' }
 ];
+``
