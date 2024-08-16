7e71d80d986f566872ce26dde08d2bce68338159
dungnt1907 <dungnt1@bitecco.vn>
2024-08-16 17:27:04 +0700

    fix

---
+++ server.js
@@ -0,0 +1,42 @@
+/* server.js in root directory */
+const fs = require('fs');
+const path = require('path');
+
+const dir = "src/environments";
+const file = "environment.ts";
+const prodFile = "environment.development.ts"; // For production deployment
+
+const content = `${process.env.JEXCEL_LICENSE}`;
+
+fs.access(dir, fs.constants.F_OK, async (err) => {
+  if (err) {
+    // Directory doesn't exist
+    console.log("src doesn't exist, creating now", process.cwd());
+    // Create /src
+    try {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    catch (error) {
+      console.log(`Error while creating ${dir}. Error is ${error}`);
+      process.exit(1);
+    }
+  }
+  // Now write to file
+  try {
+    const fileContent = fs.readFileSync(dir + '/' + file, { encoding: 'utf8' });
+    const value = fileContent.replace(/#JEXCEL_LICENSE/i, content);
+    console.log(value);
+
+    // fs.writeFileSync(dir + "/" + file, content);
+    fs.writeFileSync(dir + "/" + prodFile, value);
+    console.log("Created successfully in", process.cwd());
+    if (fs.existsSync(dir + "/" + file)) {
+      console.log("File is created", path.resolve(dir + "/" + file));
+      const str = fs.readFileSync(dir + "/" + file).toString();
+      console.log(str);
+    }
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+});
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  jexcelLicense: '#JEXCEL_LICENSE',
+};
