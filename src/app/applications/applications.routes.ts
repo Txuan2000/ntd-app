36cb7390ae90354d78f99241a61445fa6e9fcbe7
Dung Nguyen <dungnt1@bitecco.vn>
2024-11-08 09:41:05 +0700

    add code editor

--- src/app/applications/applications.routes.ts
+++ src/app/applications/applications.routes.ts
@@ -3,11 +3,13 @@ import { GenTextComponent } from './gen-text/gen-text.component';
 import { NoterComponent } from './noter/noter.component';
 import { DexcelComponent } from './dexcel/dexcel.component';
 import { DistanceMapComponent } from './distance-map/distance-map.component';
+import { CodeEditorComponent } from './code-editor/code-editor.component';
 
 export const routes: Routes = [
   { path: '', component: DistanceMapComponent },
   { path: 'gen-text', component: GenTextComponent },
   { path: 'noter', component: NoterComponent },
   { path: 'dexcel', component: DexcelComponent },
+  { path: 'code-editor', component: CodeEditorComponent },
   { path: '**', redirectTo: 'applications' },
 ];
---
+++ src/app/applications/code-editor/code-editor.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-editor.component.html',
+})
+export class CodeEditorComponent {
+
+}
